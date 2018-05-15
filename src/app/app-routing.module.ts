ebdd2758f7618661b8de4ff7320f137f0e798d6c
Adler Luiz <adlerluiz10@gmail.com>
2018-05-14 21:03:56 -0300

    some features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,7 +32,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { useHash: true, enableTracing: false } ) ],
+    imports: [RouterModule.forRoot(routes, { useHash: false, enableTracing: false } ) ],
     exports: [RouterModule]
 })
 export class AppRoutingModule {
